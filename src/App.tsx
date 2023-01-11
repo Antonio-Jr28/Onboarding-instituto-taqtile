db046316a5117954255586d77d8b1764c5d13c02
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2023-01-11 17:50:58 -0300

    installing eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-function App() {
+export function App() {
   return (
     <div>
       Hello World
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+
