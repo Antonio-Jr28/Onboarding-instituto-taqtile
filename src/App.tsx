bff4bcff1e675b3249c033f1609b894e6bbd763d
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2023-01-12 12:50:04 -0300

    create input email and password

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import { Title } from "./components/title";
+import { Input } from './components/input/input'
 
 export function App() {
   return (
+    <>
     <Title />
+    <Input />
+    </>
   )
 }
 
---
+++ src/components/input/input.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import { InputEmail, InputPassword } from './style-input';
+
+export const Input = ():JSX.Element => <><InputEmail></InputEmail> <InputPassword></InputPassword></>;
+
