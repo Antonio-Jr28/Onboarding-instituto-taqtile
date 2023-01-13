128f63d67005782cb2cd1f3353be70b35c501534
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2023-01-13 15:48:23 -0300

    adjusting button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { ButtonStyle } from './style-button';
+import { ButtonStyle } from './button-style';
 
 export const Button = ():JSX.Element => <ButtonStyle>Enviar</ButtonStyle>;
 
