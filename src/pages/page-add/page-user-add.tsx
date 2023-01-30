e5bd0a07ee6402027ea25d3ebb24a76f91016208
Äntonio-Jr28 <goncalvesantonio.jr@gmail.com8>
2023-01-30 14:40:45 -0300

    creating add user page and route

---
+++ src/pages/page-add/page-user-add.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Title } from "./page-user-add-style";
+
+export const PageAdd = ():JSX.Element => 
+    <Title>
+        Adicionar Usuario
+    </Title>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { PageAdd } from "../pages/page-add/page-user-add";
 import { PageLogin } from "../pages/page-login/page-login";
 import { PageUser } from "../pages/page-user/page-user";
 
@@ -10,6 +11,7 @@ export const MyRoutes = () => {
             <Routes>
                 <Route path="/" element={<PageLogin/>} />
                 <Route path="/pageuser/:offset" element={<PageUser /> } />
+                <Route path="/usercreate" element={<PageAdd />} />
             </Routes>
         </BrowserRouter>
     )
