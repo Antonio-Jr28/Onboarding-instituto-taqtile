fc7a8e02853df1bcfceebe6a76210c25d1370a48
Äntonio-Jr28 <goncalvesantonio.jr@gmail.com8>
2023-01-30 23:02:13 -0300

    creating page details, route and request

---
+++ src/pages/page-details/datails-user.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Title } from "../../components/add/add-user-style";
+
+export const DatailsUser = ():JSX.Element => {
+
+    return (
+        <Title>Olá mundo!</Title>
+
+    );
+};
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PageAdd } from "../pages/page-add/page-user-add";
 import { PageLogin } from "../pages/page-login/page-login";
 import { PageUser } from "../pages/page-user/page-user";
+import { DatailsUser } from "../pages/page-details/datails-user";
 
 export const MyRoutes = () => {
     return (
@@ -12,6 +13,7 @@ export const MyRoutes = () => {
                 <Route path="/" element={<PageLogin/>} />
                 <Route path="/pageuser/*" element={<PageUser /> } />
                 <Route path="/usercreate" element={<PageAdd />} />
+                <Route path="/datailsuser/:id" element={<DatailsUser/>} />
             </Routes>
         </BrowserRouter>
     )
